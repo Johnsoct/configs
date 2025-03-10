1bb14973fb6c0e9d79cf5d02fbf68dc256034951
Chris Johnson <johnsoctbus@gmail.com>
2025-03-10 09:52:28 -0400

    migrated from flatcompat to flatconfig

--- eslint/eslint.config.js
+++ eslint/eslint.config.js
@@ -1,51 +1,10 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import typescriptPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import cypress from 'eslint-plugin-cypress';
-import globals from 'globals';
-import path from "path";
-import { fileURLToPath } from "url";
+import eslint from '@eslint/js'
+import globals from 'globals'
+import perfectionist from 'eslint-plugin-perfectionist'
+import stylistic from '@stylistic/eslint-plugin'
+import typescript from 'typescript-eslint'
+import vue from 'eslint-plugin-vue'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-});
-const cypressConfig = {
-    files: [
-        'cypress/**/*.{cy,spec}.{ts,jsx,tsx}',
-        'cypress/**/*.{js,ts,jsx,tsx}',
-        'src/**/*.{cy,spec}.*',
-        'examples/lint-examples.unit.spec.js',
-    ],
-    languageOptions: {
-        globals: {
-            after: 'readonly',
-            afterEach: 'readonly',
-            before: 'readonly',
-            beforeEach: 'readonly',
-            context: 'readonly',
-            cy: 'readonly',
-            Cypress: 'readonly',
-            describe: 'readonly',
-            expect: 'readonly',
-            it: 'readonly',
-        },
-        parser: typescriptParser,
-        parserOptions: {
-            project: 'cypress/tsconfig.json',
-        },
-    },
-    plugins: {
-        "@typescript-eslint": typescriptPlugin,
-        cypress,
-    },
-    rules: {
-        ...cypress.configs.recommended.rules,
-    },
-};
 const jsRules = {
     '@stylistic/js/array-bracket-spacing': ['warn', 'always'],
     '@stylistic/js/arrow-parens': [2, "as-needed", { "requireForBlockBody": true }],
@@ -86,109 +45,122 @@ const jsRules = {
     'no-useless-escape': 'warn',
     'prefer-object-spread': 'error',
 };
+const tsRules = {
+    ...jsRules,
+    '@typescript-eslint/no-unused-vars': 'off',
+    'perfectionist/sort-imports': ['off', {
+        groups: [
+            ['builtin', 'external'],
+            ['internal'],
+            ['parent', 'siblings', 'side-effect'],
+            ['side-effect-style'],
+        ],
+        'newlines-between': 'ignore',
+    }],
+    'perfectionist/sort-vue-attributes': 'off',
+}
+const vueRules = {
+    '@stylistic/js/indent': 'off', // Turn off in favor of 'vue/script-indent'
+    'vue/attributes-order': 'off',
+    'vue/html-indent': ['error', 4, {
+        'attribute': 1,
+        'alignAttributesVertically': true,
+        'baseIndent': 1,
+    }],
+    'vue/html-closing-bracket-newline': ['error', {
+        'singleline': 'never',
+        'multiline': 'always',
+        "selfClosingTag": {
+            "singleline": "never",
+            "multiline": "always"
+        }
+    }],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-side-effects-in-computed-properties': 'warn',
+    'vue/no-v-html': 'warn', // Warn (where we're using it, it is safe)
+    'vue/script-indent': ['error', 4, {
+        'baseIndent': 0,
+    }],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/valid-template-root': 'off',
+    'vue/valid-v-for': 'off',
+}
+const cypressConfig = {
+    files: [
+        'cypress/**/*.{cy,spec}.{ts,jsx,tsx}',
+        'cypress/**/*.{js,ts,jsx,tsx}',
+        'src/**/*.{cy,spec}.*',
+        'examples/lint-examples.unit.spec.js',
+    ],
+    languageOptions: {
+        globals: {
+            after: 'readonly',
+            afterEach: 'readonly',
+            before: 'readonly',
+            beforeEach: 'readonly',
+            context: 'readonly',
+            cy: 'readonly',
+            Cypress: 'readonly',
+            describe: 'readonly',
+            expect: 'readonly',
+            it: 'readonly',
+        },
+        parserOptions: {
+            parser: typescriptParser,
+            project: 'cypress/tsconfig.json',
+        },
+    },
+    plugins: {
+        cypress,
+    },
+    rules: {
+        ...cypress.configs.recommended.rules,
+    },
+};
 const globalConfig = {
     ignores: [
         '**/dist/**/*',
         '**/node_modules/**/*',
     ],
     languageOptions: {
+        ecmaVersion: 2022,
         globals: {
             ...globals.browser,
         },
-        parserOptions: {
-            ecmaVersion: 2022,
-            sourceType: 'module',
-        },
+        sourceType: 'module',
     },
 };
-const tsConfig = compat.config({
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:perfectionist/recommended-alphabetical',
+const tsConfig = {
+    ...eslint.configs.recommended,
+    ...typescript.configs.recommended,
+    ...vue.configs['flat/recommended'],
+    files: [ "**/*.{js,mjs,cjs,ts,vue" ],
+    ignores: [
+        "**/cypress",
+        '*.d.ts',
+        '**/coverage',
+        '**/dist',
+        '*.config.{js,ts}',
     ],
-    ignorePatterns: [
-        "cypress",
-        'cypress.config.ts',
-        'eslint.config.js',
-        'env.d.ts',
-        'modules.d.ts',
-        'shim-vue.d.ts',
-        'toastr.d.ts',
-        'vite.config.ts',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        extraFileExtensions: ['.vue'],
-        project: true,
-        tsconfigRootDir: __dirname,
-    },
-    plugins: ['@stylistic/js'],
-    rules: {
-        ...js.configs.recommended.rules,
-        ...jsRules,
-        '@typescript-eslint/no-unused-vars': 'off',
-        'perfectionist/sort-imports': ['off', {
-            groups: [
-                ['builtin', 'external'],
-                ['internal'],
-                ['parent', 'siblings', 'side-effect'],
-                ['side-effect-style'],
-            ],
-            'newlines-between': 'ignore',
-        }],
-        'perfectionist/sort-vue-attributes': 'off',
-    },
-});
-const vueConfig = compat.config({
-    env: {
-        es2022: true,
-        node: true,
-    },
-    extends: [
-        'plugin:vue/base',
-        'plugin:vue/vue3-essential',
-        'plugin:vue/vue3-strongly-recommended',
-        'plugin:vue/vue3-recommended',
-    ],
-    parser: "vue-eslint-parser",
-    parserOptions: {
+    languageOptions: {
         ecmaVersion: 2022,
-        parser: "@typescript-eslint/parser",
+        globals: globals.browser,
+        parserOptions: {
+            parser: typescript.parser,
+        },
         sourceType: "module",
     },
-    plugins: ['vue'],
-    rules: {
-        '@stylistic/js/indent': 'off', // Turn off in favor of 'vue/script-indent'
-        'vue/attributes-order': 'off',
-        'vue/html-indent': ['error', 4, {
-            'attribute': 1,
-            'alignAttributesVertically': true,
-            'baseIndent': 1,
-        }],
-        'vue/html-closing-bracket-newline': ['error', {
-            'singleline': 'never',
-            'multiline': 'always',
-            "selfClosingTag": {
-                "singleline": "never",
-                "multiline": "always"
-            }
-        }],
-        'vue/multi-word-component-names': 'off',
-        'vue/no-side-effects-in-computed-properties': 'warn',
-        'vue/no-v-html': 'warn', // Warn (where we're using it, it is safe)
-        'vue/script-indent': ['error', 4, {
-            'baseIndent': 0,
-        }],
-        'vue/singleline-html-element-content-newline': 'off',
-        'vue/valid-template-root': 'off',
-        'vue/valid-v-for': 'off',
+    plugins: {
+        perfectionist,
+        '@stylistic': stylistic,
     },
-});
+    rules: {
+        ...tsRules,
+        ...vueRules,
+    },
+};
 
 export default [
     globalConfig,
-    cypressConfig,
-    ...tsConfig,
-    ...vueConfig,
+    tsConfig,
 ];
