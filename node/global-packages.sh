3beb16646ef2fa50d1c2ff924c12983e76ca82bc
Johnson <johnsoct@hey.com>
2025-11-02 13:13:35 -0500

    added node and global-packages install script

---
+++ node/global-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm i -g @vue/language-server @vue/typescript-plugin corepack css-variables-language-server eslint eslint_d fixjson htmlhint jiti jsonlint markdownlint postcss-html sass stylelint stylelint-config-standard stylelint-config-standard-scss stylelint-lsp tldr typescript typescript-language-server vscode-css-languageservice
