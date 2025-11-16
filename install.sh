07c7cf70ad0e13703361d19948cf22f0a3bdc883
Taylor <johnsoct@hey.com>
2025-11-16 08:38:12 -0500

    testing alacritty

--- install.sh
+++ install.sh
@@ -10,23 +10,23 @@ git pull
 ln -s ./alacritty ~/.config
 
 # Bash
-ln -s ./bash/.bashrc ~
-ln -s ./bash/.bash_aliases ~
-
-# Fonts
-ln -s ./fontpatcher/DankMonoNerdFont-Regular.ttf ~/.local/share/fonts
-ln -s ./fontpatcher/DankMonoNerdFontPlusCodicons-Regular.ttf ~/.local/share/fonts
-
-# Nvim
-ln -s ./nvim ~/.config
-
-# Vim
-ln -s ./vim/.vimrc ~
-
-# Zellij
-ln -s ./zellij ~/.config
+# ln -s ./bash/.bashrc ~
+# ln -s ./bash/.bash_aliases ~
+#
+# # Fonts
+# ln -s ./fontpatcher/DankMonoNerdFont-Regular.ttf ~/.local/share/fonts
+# ln -s ./fontpatcher/DankMonoNerdFontPlusCodicons-Regular.ttf ~/.local/share/fonts
+#
+# # Nvim
+# ln -s ./nvim ~/.config
+#
+# # Vim
+# ln -s ./vim/.vimrc ~
+#
+# # Zellij
+# ln -s ./zellij ~/.config
 
 # Additional Installs
 # Additional Installs
 # Additional Installs
-./node/global-packages.sh
+# ./node/global-packages.sh
