712b51542a9c3577ba67011bceb4929a20982819
Taylor <johnsoct@hey.com>
2025-11-16 08:36:44 -0500

    updated install script with necessary symlinks

--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 git pull
 
-ln -s ./test.txt ~
+# TODO: convert into a loop via an array
+
+# Symlinks
+# Symlinks
+# Symlinks
+
+# Alacritty
+ln -s ./alacritty ~/.config
+
 # Bash
-# ln -s ~ ./bash/.bashrc
-# ln -s ~ ./bash/.bash_aliases
+ln -s ./bash/.bashrc ~
+ln -s ./bash/.bash_aliases ~
+
+# Fonts
+ln -s ./fontpatcher/DankMonoNerdFont-Regular.ttf ~/.local/share/fonts
+ln -s ./fontpatcher/DankMonoNerdFontPlusCodicons-Regular.ttf ~/.local/share/fonts
+
+# Nvim
+ln -s ./nvim ~/.config
+
+# Vim
+ln -s ./vim/.vimrc ~
+
+# Zellij
+ln -s ./zellij ~/.config
+
+# Additional Installs
+# Additional Installs
+# Additional Installs
+./node/global-packages.sh
